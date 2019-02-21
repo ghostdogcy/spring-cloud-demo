a2281897c476103828c40dc14af1053e35ec5250
ghostdogcy <ghostdogcy@163.com>
2019-02-21 15:16:21 +0800

    commit config client

---
+++ config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -0,0 +1,32 @@
+package com.example.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableEurekaClient
+@SpringBootApplication
+@RestController
+@RefreshScope
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+
+    @Value("${Hello}")
+    String Hello;
+
+    @RequestMapping("/Hello")
+    public String hello()
+    {
+        return Hello;
+//        return "hello";
+    }
+
+}
